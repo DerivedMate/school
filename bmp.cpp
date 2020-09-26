56fae2c3900d40c67284fb2769bf728cff3083d8
derivedmate <tosurowiec@gmail.com>
2020-09-26 17:05:56 +0200

    a tiny refactoring

--- bmp.cpp
+++ bmp.cpp
@@ -5,89 +5,133 @@
 #include <math.h>
 #include <vector>
 
-typedef unsigned char header[54];
-typedef unsigned char pixels[][3];
-typedef std::vector<unsigned char> excess;
+typedef unsigned char uchar;
 
-const unsigned char mx = 255;
-const unsigned char mn = 0;
+struct Pixel {
+  uchar r;
+  uchar g;
+  uchar b;
 
-void read_header(std::ifstream& f, unsigned char* header) {
+  Pixel(uchar r, uchar g, uchar b) {
+    this->r = r;
+    this->g = g;
+    this->b = b;
+  }
+
+  bool operator== (Pixel& p) {
+    return this->r == p.r && this->g == p.g && this->b == p.b;
+  }
+};
+
+typedef unsigned char headerT[54];
+typedef std::vector<Pixel> pixelsT;
+typedef std::vector<unsigned char> excessT;
+
+void read_header(std::ifstream& f, uchar* header) {
   for(int i = 0; i < 54 && f >> header[i]; i++) {}
 }
 
-void read_pixels(std::ifstream& f, pixels pixels, int size) {
+void read_pixels(std::ifstream& f, pixelsT& pixels, int size) {
   for(int i = 0; i < size; i++) {
-    unsigned char g, b, r;
+    uchar g, b, r;
     f >> g >> b >> r;
+    Pixel p(r, g, b);
 
-    pixels[i][0] = r;
-    pixels[i][1] = g;
-    pixels[i][2] = b;
+    pixels.push_back(p);
   }
 }
 
-bool are_pixels_eq(pixels px, int i, int j) {
-  int r0 = px[i][0],
-      g0 = px[i][1],
-      b0 = px[i][2];
+void read_excess(std::ifstream& f, excessT& ex) {
+  uchar u;
+  while(f >> u) ex.push_back(u);
+}
 
-  int r1 = px[j][0],
-      g1 = px[j][1],
-      b1 = px[j][2];
+struct Image {
+  int w;
+  int h;
+  headerT header;
+  pixelsT pixels;
+  excessT excess;
+  
+  Image(std::string path) {
+    std::ifstream f(path);
+    read_header(f, this->header);
+    this->w = this->header[18];
+    this->h = this->header[22];
 
-  bool eq = r0 == r1 && g0 == g1 && b0 == b1;
+    read_pixels(f, this->pixels, this->size());
+    read_excess(f, this->excess);
 
-  if(!eq) printf("(%i, %i, %i) /= (%i, %i, %i); %i /= %i\n", r0, g0, b0, r1, g1, b1, i, j);
+    f.close();
+  }
+
+  int size() {
+    return this->w * this->h;
+  }
+};
+
+const uchar mx = 255;
+const uchar mn = 0;
+
+bool are_pixels_eq(pixelsT& px, int i, int j) {
+  Pixel p0 = px.at(i);
+  Pixel p1 = px.at(j);
+
+  bool eq = p0 == p1;
+
+  if(!eq) printf("(%i, %i, %i) /= (%i, %i, %i); %i /= %i\n", p0.r, p0.g, p0.b, p1.r, p1.g, p1.b, i, j);
 
   return eq;
 }
 
-bool is_symmetric(pixels pixels, int size, int w, int h) {
+bool is_symmetric(pixelsT pixelsT, int size, int w, int h) {
   for(int i = 0; i < size/2; i++) {
     int y = i / w;
     int x = i % w;
     int j = (h - y - 1) * w + x;
 
-    if(!are_pixels_eq(pixels, i, j)) {
-        return false;
+    if(!are_pixels_eq(pixelsT, i, j)) {
+      return false;
     };
   }
   return true;
 }
 
-void print_gradient(header header, excess& excess, int w, int h, int size) {
+void print_gradient(headerT headerT, excessT& excess, int w, int h, int size) {
   std::ofstream out("gradient.bmp");
   // print the header
-  for(int i = 0; i < 54; i++) {
-    out << header[i];
-  }
+  for(int i = 0; i < 54; i++) out << headerT[i];
 
   for(int i = 0; i < size; i ++) {
-    int           x     = i % w;
-    int           y     = i / w;
+    int    x      = i % w;
+    int    y      = i / w;
+    int    a      = w/2;
+    int    b      = h/2;
+    int    x_r    = x - a;
+    int    y_r    = y - b;
 
-    double        s     = double(x*y) / double(w*w);
-    unsigned char red   = s * 255,
-                  green = s * 100 + 100,
-                  blue  = s * 255;
+    double s      = std::abs(double(x_r*y_r) / double((w-1)*(h-1)));
+           s      = std::cbrt(s);
+    double red0   = 252, red1   = 3  ;
+    double green0 = 219, green1 = 252; 
+    double blue0  = 3  , blue1  = 248;
 
-    out << green << blue << red;
+    uchar  red    = s * (red1   - red0)   + red0,
+           green  = s * (green1 - green0) + green0,
+           blue   = s * (blue1  - blue0)  + blue0;
+
+    out << blue << green << red;
   }
 
   // print the rest of the input file
-  for(unsigned char u : excess) {
-    out << u;
-  }
+  for(uchar u : excess) out << u;
 
   out.close();
 }
 
-void print_flag(std::ofstream& out, pixels px, header header, excess& excess, int w, int h, int size) {
+void print_flag(std::ofstream& out, pixelsT px, headerT headerT, excessT& excess, int w, int h, int size) {
   // print the header
-  for(int i = 0; i < 54; i++) {
-    out << header[i];
-  }
+  for(int i = 0; i < 54; i++) out << headerT[i];
 
   // print the flag
   for(int i = 0; i < size; i++) {
@@ -101,21 +145,10 @@ void print_flag(std::ofstream& out, pixels px, header header, excess& excess, in
   }
 
   // print the rest of the input file
-  for(unsigned char u : excess) {
-    out << u;
-  }
+  for(uchar u : excess) out << u;
 }
 
-bool is_circle(int i, int w, int h, int a, int b, int r) {
-  int x  = i % w;
-  int y  = i / w;
-  int dx = x - a,
-      dy = y - b;
-
-  return dx*dx + dy*dy <= r*r;
-}
-
-void print_gif(int n, int w, int h, int size, header header, excess& excess) {
+void print_gif(int n, int w, int h, int size, headerT headerT, excessT& excess) {
   std::string f_name = "./gif-src/part";
   f_name += std::to_string(n);
   f_name += ".bmp";
@@ -125,18 +158,18 @@ void print_gif(int n, int w, int h, int size, header header, excess& excess) {
       b = 0,
       r = n + 1;
 
-  for(int i = 0; i < 54; i++) out << header[i];
+  for(int i = 0; i < 54; i++) out << headerT[i];
 
   for(int i = 0; i < size; i++) {
     int x  = i % w - w / 2;
     int y  = i / w - h / 2;
-    int dx = x-a,
-        dy = y-b;
+    int dx = x - a,
+        dy = y - b;
 
-    float dist          = dx*dx + dy*dy;
+    float dist          = dx * dx + dy * dy;
     float r_pr          = dist / r;
     unsigned char red   = r_pr * 255,
-        green           = r_pr*r_pr * 100,
+        green           = r_pr * r_pr * 100,
         blue            = r_pr / 2 * 24 + 100 % 255;
       
     out << green << blue << red;
@@ -147,40 +180,25 @@ void print_gif(int n, int w, int h, int size, header header, excess& excess) {
   
 
   out.close();
-}
+} 
 
 int main() {
-  std::ifstream f("./blank.bmp");
-  std::ofstream out("./out.bmp");
-  header header;
-  read_header(f, header);
+  Image input("./inp2.bmp");
+  Image blank("./blank.bmp");
+  Image blank_flag("./blank_flag.bmp");
+  std::ofstream out("out.bmp");
 
-  int width  = (int)header[18];
-  int height = (int)header[22];
-  int size   = width * height;
+  std::cout << input.w << "x" << input.h << " : " << input.size() << std::endl;
+  std::cout << "symmetric: " << (is_symmetric(input.pixels, input.size(), input.w, input.h) ? "yep" : "no") << std::endl;
 
-  auto pixels = new unsigned char[size][3];
-  read_pixels(f, pixels, size);
 
-  std::cout << width << " " << height << " " << size << std::endl;
-  std::cout << "symmetric: " << (is_symmetric(pixels, size, width, height) ? "yep" : "no") << std::endl;
-
-  excess excess; 
-  {
-    unsigned char u;
-    while (f >> u) excess.push_back(u);
-  }
-
-  print_flag(out, pixels, header, excess, width, height, size);
+  print_flag(out, blank_flag.pixels, blank_flag.header, blank_flag.excess, blank_flag.w, blank_flag.h, blank_flag.size());
   
-  for(int i = 0; i < 500; i++) {
-    print_gif(i, width, height, size, header, excess);
+  for(int i = 0; i < 800; i++) {
+    print_gif(i, blank.w, blank.h, blank.size(), blank.header, blank.excess);
   }
 
-  print_gradient(header, excess, width, height, size);
-
-  f.close();
-  out.close();
+  print_gradient(blank.header, blank.excess, blank.w, blank.h, blank.size());
 
   return 0;
 }
--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
-rm ./gif-src/*;
+if [ ! -d gif-src ]; then 
+  mkdir gif-src
+  echo "Created gif-src"
+fi;
+
+len=`ls ./gif-src | wc -l`
+if [ "${len}" -gt 0 ] ; then 
+  rm gif-src/*
+  echo "Removed outdated part files"
+fi;
 
 FILE="./exe.o";
 g++ -o $FILE $1 && chmod +x $FILE && $FILE;
 
+echo
 cd gif-src;
 convert -delay 8 -loop 0 `ls -v` ../the-gif.gif 
